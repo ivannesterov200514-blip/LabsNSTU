8d5a5e09b5a9003ac76e22552b41317574c57132
NesterovIA_ABs-525 <ivannesterov200514@gmail.com>
2026-06-01 23:16:41 +0700

    upd

---
+++ pr2/task3/task3.h
@@ -0,0 +1,11 @@
+#ifndef TASK3_H
+#define TASK3_H
+
+#include <iostream>
+#include <stdint.h>
+using namespace std;
+
+int64_t gcd_euclidext(int64_t a, int64_t b, int64_t& u, int64_t& v);
+int64_t modinv(int64_t c, int64_t m);
+
+#endif
---
+++ pr2/task6/main.cpp
@@ -0,0 +1,37 @@
+#include "cep_drob.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int64_t A = 1256;
+    int64_t B = 847;
+    int64_t D = 119;
+
+    cout << "\n" << A << "a + " << B << "b = " << D << "\n";
+
+    int64_t u, v;
+    pair<int64_t, vector<int64_t>> nums = gcd_euclidext(A, B, u, v);
+
+    cout << "\nЦепная дробь A/B: [ ";
+    for (int64_t q : nums.second) cout << q << " ";
+    cout << "]\n";
+
+    cout << "Тождество Безу: " << A << "*(" << u << ") + " << B << "*(" << v << ") = " << nums.first << "\n";
+
+    // Проверка разрешимости
+    if (D % nums.first != 0) {
+        cout << "Уравнение не имеет решений в целых числах (НОД не делит D).\n";
+        return 0;
+    }
+
+    pair<int64_t, int64_t> sol = priv_sol(D, nums.first, u, v);
+
+    cout << "\nЧастное решение: a0 = " << sol.first << ", b0 = " << sol.second << "\n";
+    cout << "Проверка: " << A << "*" << sol.first << " + " << B << "*" << sol.second << " = " << A * sol.first + B * sol.second << " (должно быть " << D << ")\n";
+
+    // Общее решение
+    cout << "\nОбщее решение:\n";
+    cout << "a = " << sol.first << " + " << (B / nums.first) << "*t\n";
+    cout << "b = " << sol.second << " - " << (A / nums.first) << "*t\n";
+    return 0;
+}
